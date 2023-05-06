32f6b78c44fd4a390113c3d8208fdc96e4397b03
Emon <emon.mhk69@gmail.com>
2023-05-06 07:46:54 +0600

    post method executed done

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require(`express`);
 const app = express();
 const port  = process.env.PORT || 5000;
+
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 const users = [
     {id: 1 , name: "Emon" , email: "emon.mhk69@gmail.com"},
     {id: 2 , name: "Elina" , email: "elina.jsr05@gmail.com"},
-    {id: 3 , name: "farjana" , email: "farju.brs@gmail.com"}
+    {id: 3 , name: "Emolina" , email: "emolina@gmail.com"}
 ]
 
 app.get("/" , (req, res)=>{
@@ -16,6 +18,15 @@ app.get("/" , (req, res)=>{
 
 app.get("/users" , (req, res)=>{
     res.send(users);
+})
+
+app.post("/users" , (req,res)=>{
+
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
+
 })
 
 app.listen(port , ()=>{
